593f6335867013d2c6d6a653c21b04a1e90c45b0
Yumeinaruu <lisovoistas@yandex.ru>
2024-03-05 21:39:55 +0300

    spring core java config

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -1,14 +1,13 @@
 package com.tms;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("MySettings.xml");
-        Cap capBean = (Cap) context.getBean("cap");
-        System.out.println(capBean.hashCode());
-
-
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+        Cap capBean = (Cap) context.getBean("getCapBean");
+        System.out.println(capBean);
     }
 }
---
+++ src/main/java/com/tms/SpringConfig.java
@@ -0,0 +1,13 @@
+package com.tms;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //этот класс используется для настройки приложения
+public class SpringConfig {
+
+    @Bean
+    public Cap getCapBean(){
+        return new Cap("from Java");
+    }
+}
